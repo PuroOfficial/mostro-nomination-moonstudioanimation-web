496ce7c50449fdd0035e52e91201a75efb965bc3
Puro <79566743+PuroOfficial@users.noreply.github.com>
2025-08-22 22:53:38 +0900

    Error Format fixes

--- pages/408.jsx
+++ pages/408.jsx
@@ -1,9 +1,9 @@
 function Error() {
     return (
-        <body>
-                <h1>hello if you are seeing this, voting is down.</h1>
-                <br>
-                <p>don't worry! maybe the website is doing some difficult tasks that take processing power.</p>
-        </body>
+        <h1>hello if you are seeing this, voting is down.</h1>
+        <br>
+        <p>don't worry! maybe the website is doing some difficult tasks that take processing power.</p>
     );
 }
+
+export default Error;
