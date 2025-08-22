5d6f5462f4c91dafec0070897692906080b5e488
Puro <79566743+PuroOfficial@users.noreply.github.com>
2025-08-23 01:02:26 +0900

    symbol that self-destructs has been removed
    wtf is it trying to grab from?
    /&/

--- pages/index.jsx
+++ pages/index.jsx
@@ -280,7 +280,7 @@ export default function VotingSystem() {
         {/* Embedded font styles */}
         <style>
           {`
-            @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700');
             
             body {
               background-color: black;
